24049850b61c03d0ec49204aa705066c4a09b64f
EAJ <eaj9990@gmail.com>
2023-03-21 04:31:36 -0700

    add test cases, and comments

--- main.c
+++ main.c
@@ -1,5 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+/*
+In this assignment, we will focus on the world's most popular card game - poker.
+This game is known in many variants. Here we will deal with probably the most widespread,
+Texas Hold'em Poker, when each player holds two cards in his hand and (up to)
+five cards common to all players are dealt to the table.
+Your task will be to write a program that will communicate with the user via the
+command line. Of course, your program will need to be able to compare individual
+card combinations with each other. The heart of the problem will be the algorithm
+deciding which of the two players won, if it knows both cards of both players
+and all five cards lying on the table.
+
+
+Test case 1: Player 1 wins with a straight
+TC KH
+3H 9D
+9C QD JS 3C 2H
+
+Test case 2: Player 2 wins with a flush
+8S 9S
+KH QS
+2S 7S 4S 5D 6D
+
+Test case 3: Draw with a full house for both players
+KC KD
+KH KS
+QC QD QS 2C 2S
+
+Test case 4: Player 1 wins with a higher pair
+AC 5D
+KC 9C
+AD 3H 6D 8C 7H
+
+Test case 5: Player 2 wins with a two-pair
+8C 8D
+6H 6D
+7H 7D 9S 2C 3S
+ */
+
+typedef struct {
+    int value;
+    char suit;
+} Card;
+
+int card_value(char card_value);
+
+int compare_hands(Card *player1, Card *player2, Card *community);
 
 
 /* Parse the number of players at the table for the bonus extension.
